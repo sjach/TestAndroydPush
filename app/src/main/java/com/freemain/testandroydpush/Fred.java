bc6e76e5d33874ca84fbd989d1063be728dd688f
sjach <steve.jachim@gmail.com>
2016-08-19 00:42:15 +0100

    added fred class update mainactivity

--- app/src/main/java/com/freemain/testandroydpush/Fred.java
+++ app/src/main/java/com/freemain/testandroydpush/Fred.java
@@ -2,13 +2,32 @@ package com.freemain.testandroydpush;
 
 /**
  * Created by steve on 05/08/16.
+ *
+ * This is a helper class that just does nothing.
  */
 public class Fred {
 
     private int x;
 
+    /**
+     * init method
+     */
     Fred()
     {
         x = 1;
+    }
+
+
+    /**
+     * this is about eric ok
+     * @param x is an integer
+     * @return a bigger  integer
+     */
+
+    int eric(int x)
+    {
+        int y = x;
+
+        return y + 2;
     }
 }
